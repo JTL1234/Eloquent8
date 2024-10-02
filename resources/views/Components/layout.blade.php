d33fced90ce584b4de64fb8b092467db88bdcfe2
JTL1234 <limpagjethro@gmail.com>
2024-10-03 01:40:50 +0800

    0921_09_Meet_Eloquent

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -1,22 +1,23 @@
 <!doctype html>
-<html lang="en" class="h-full bg-gray-100">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Website</title>
+    <title>My JTL Website</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="h-full">
+<body>
+
 <div class="min-h-full">
     <nav class="bg-gray-800">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div class="flex h-16 items-center justify-between">
                 <div class="flex items-center">
                     <div class="flex-shrink-0">
-                        <img class="h-8 w-8" src="https://laracasts.com/images/logo/logo-triangle.svg" alt="Your Company">
+                        <img class="h-8 w-8 rounded-full" src="https://img.freepik.com/premium-photo/cat-logo-icon-silhouette-vector-style-with-white-background_1047546-8007.jpg" alt="Your Company">
                     </div>
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
@@ -36,28 +37,28 @@
                             </svg>
                         </button>
 
-                        <!-- Profile dropdown -->
+             
                         <div class="relative ml-3">
                             <div>
                                 <button type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                     <span class="absolute -inset-1.5"></span>
                                     <span class="sr-only">Open user menu</span>
-                                    <img class="h-8 w-8 rounded-full" src="https://laracasts.com/images/lary-ai-face.svg" alt="">
+                                    <img class="h-8 w-8 rounded-full" src="https://seeklogo.com/images/E/eevee-logo-D6A54F96A5-seeklogo.com.png" alt="">
                                 </button>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="-mr-2 flex md:hidden">
-                    <!-- Mobile menu button -->
+          
                     <button type="button" class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" aria-controls="mobile-menu" aria-expanded="false">
                         <span class="absolute -inset-0.5"></span>
                         <span class="sr-only">Open main menu</span>
-                        <!-- Menu open: "hidden", Menu closed: "block" -->
+                 
                         <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                         </svg>
-                        <!-- Menu open: "block", Menu closed: "hidden" -->
+
                         <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
@@ -66,10 +67,10 @@
             </div>
         </div>
 
-        <!-- Mobile menu, show/hide based on menu state. -->
+      
         <div class="md:hidden" id="mobile-menu">
             <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+          
                 <a href="/" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Home</a>
                 <a href="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About</a>
                 <a href="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contact</a>
@@ -77,11 +78,11 @@
             <div class="border-t border-gray-700 pb-3 pt-4">
                 <div class="flex items-center px-5">
                     <div class="flex-shrink-0">
-                        <img class="h-10 w-10 rounded-full" src="https://laracasts.com/images/lary-ai-face.svg" alt="">
+                        <img class="h-10 w-10 rounded-full" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F65-creative-cat-logo-design-ideas-for-your-inspiration--315603886389553228%2F&psig=AOvVaw3pyShCOhhRxUkdfGfLsHrE&ust=1726388037069000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNi35PL-wYgDFQAAAAAdAAAAABAb" alt="">
                     </div>
                     <div class="ml-3">
-                        <div class="text-base font-medium leading-none text-white">Lary Robot</div>
-                        <div class="text-sm font-medium leading-none text-gray-400">jeffrey@laracasts.com</div>
+                        <div class="text-base font-medium leading-none text-white">JTL</div>
+                        <div class="text-sm font-medium leading-none text-gray-400">limpagjethro.com</div>
                     </div>
                     <button type="button" class="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                         <span class="absolute -inset-1.5"></span>
